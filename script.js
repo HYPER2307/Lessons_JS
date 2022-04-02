11cb2dbd2bac4e44bab4f109ae79f8e8dbc5b6e4
Adrian <yevchukadrian@gmail.com>
2022-04-02 14:33:47 +0300

    Урок 17 - Методи та властивості рядків і чисел

--- script.js
+++ script.js
@@ -1,42 +1,25 @@
 'use strict';
 
-let num = 20;
-
-function showFirstMessage(text) {
-    console.log(text);
-    num = 10;
-}
-
-showFirstMessage('Hello');
-console.log(num);
-
-// function calc (a, b) {
-//     return (a + b);
-// }
-
-// console.log(calc(2, 6));
-// console.log(calc(5, 6));
-// console.log(calc(10, 6));
+const str = 'teSt';
 
 
-function ret() {
-    let num = 50;
-    return (num);
-}
+// console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+console.log(str);
 
-const anotherNum = ret();
+const fruit = 'Some fruit';
 
-console.log(anotherNum);
+console.log(fruit.indexOf("q"));
 
-const logger = function() {
-    console.log("hello");
-};
+const logg = 'Hello world!';
 
-logger();
+// console.log(logg.slice(-6, -1));
 
-const calc = (a, b) => {
-    console.log("1");
-    return a + b;
-};
+// console.log(logg.substr(6, 5));
 
-calc(1, 5);
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+// console.log(parseInt(test));
+console.log(parseFloat(test));
