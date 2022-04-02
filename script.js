5df6fdecfdd1ef291518f0c43598654025f4c266
Adrian <yevchukadrian@gmail.com>
2022-04-02 18:17:12 +0300

    Урок 20 - Об'єкти, деструктуризація об'єктів (ES6)

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
 'use strict';
 
-function first() {
-    setTimeout(function() {
-        console.log(1);
-     } , 500);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function() {
+        console.log('test')
+    }
+};
 
-function second() {
-    console.log(2);
-}
+options.makeTest();
 
-first();
-second();
+const {border, bg} = options.colors;
+console.log(border); 
+
+// console.log(Object.keys(options).length);
+
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Властивість ${i} має значення ${options[key][i]}`);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Властивість ${key} має значення ${options[key]}`);
+//         counter++;
+//     }
+    
+// }
+
+// console.log(counter);
