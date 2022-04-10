f055c2a778a0c086ba25eee2da1e9bc40f9b1526
Adrian <yevchukadrian@gmail.com>
2022-04-10 17:29:40 +0300

    Урок 29 - Дії з елементами на сторінці

--- script.js
+++ script.js
@@ -1,23 +1,40 @@
 'use strict';
 
 const box = document.getElementById('box');
-
-console.log(box);
-
 const btns = document.getElementsByTagName('button');
-
-console.log(btns[1]);
-
 const circles = document.getElementsByClassName('circle');
-
-console.log(circles);
-
 const hearts = document.querySelectorAll('.heart');
+const oneHeart = document.querySelector('.heart');
+const wrapper = document.querySelector('.wrapper');
+
+box.style.backgroundColor = 'green';
+
+box.style.cssText = 'background-color: blue; width: 500px';
+
+btns[2].style.borderRadius = '100%';
+circles[1].style.backgroundColor = 'red';
+
+// for (let i = 0; i < hearts.length; i++) {
+//     hearts[i].style.backgroundColor = 'blue';
+// }
 
 hearts.forEach(item => {
-    console.log(item);
+    item.style.backgroundColor = 'blue';
 });
 
-const oneHeart = document.querySelector('.heart');
+ const div = document.createElement('div');
+//  const text = document.createTextNode('Sgsfbfc');
 
-console.log(oneHeart);
+div.classList.add('black');
+
+// wrapper.append(div);
+
+// wrapper.prepend(div);
+
+// hearts[0].after(div);
+
+// circles[0].remove();
+
+// hearts[0].replaceWith(circles[0]);
+
+div.insertAdjacentHTML('beforebegin', "<h1>564765</h1>");
